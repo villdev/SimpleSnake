7d3dd497781c8df18862b2c2e2391fc5e9478ee6
vill <villdevelop@gmail.com>
2019-05-21 19:39:47 +0530

    removed wall gameover2

--- SimpleSnake/SimpleSnake.cpp
+++ SimpleSnake/SimpleSnake.cpp
@@ -134,8 +134,12 @@ void Logic()
 			y++;
 			break;
 	}
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
 	if (x > width || x < 0 || y > height || y < 0)
 		gameOver = true;
+
 	for (int i = 0; i < nTail; i++)
 		if (tailX[i] == x && tailY[i] == y)
 			gameOver = true;
